a56df8de65e15ed623f737b5ed8d97cf650b1ec5
jdomnitz <380352+jdomnitz@users.noreply.github.com>
2023-06-11 22:55:41 -0400

    Add a CRC test vector from ITU-T

---
+++ UnitTests/CRC-CCITT.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using ZWaveDotNet.Security;
+using ZWaveDotNet.Util;
+
+namespace UnitTests
+{
+    public class CRC_CCITT
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Memory<byte> payload = MemoryUtil.From("C2A2150D0303020B01");
+            Memory<byte> expected = MemoryUtil.From("2C66");
+            CRC16_CCITT crc = new CRC16_CCITT();
+            byte[] result = crc.ComputeChecksum(payload);
+            CollectionAssert.AreEqual(result, expected.ToArray());
+        }
+    }
+}
