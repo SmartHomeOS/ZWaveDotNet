f81e3188015d778d1edbaffb933dcd596f4fd237
jdomnitz <380352+jdomnitz@users.noreply.github.com>
2023-06-12 19:44:04 -0400

    Add support for parsing QR Codes (S2 and SmartStart)

---
+++ UnitTests/QRCodes.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using ZWaveDotNet.CommandClasses.Enums;
+using ZWaveDotNet.Util;
+
+namespace UnitTests
+{
+    public class QRCodes
+    {
+        [Test]
+        public void Test1()
+        {
+            String QRcode = "900132782003515253545541424344453132333435212223242500100435301537022065520001000000300578";
+            Memory<byte> DSK = MemoryUtil.From("c9458a7fa1d0868d7a5b829b52e67ea9");
+            QRParser parser = new QRParser(QRcode);
+            Assert.That(parser.Version == 0x1);
+            Assert.That(parser.Keys == (SecurityKey.S2Authenticated | SecurityKey.S2Unauthenticated));
+            CollectionAssert.AreEqual(DSK.ToArray(), parser.DSK.ToArray());
+        }
+    }
+}
---
+++ ZWaveDotNet/Util/QRParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using ZWaveDotNet.CommandClasses.Enums;
+
+namespace ZWaveDotNet.Util
+{
+    public class QRParser
+    {
+        public byte Version { get; private set; }
+        private byte[] checksum = new byte[2];
+        public SecurityKey Keys { get; private set; }
+        public Memory<byte> DSK { get; private set; }
+
+        public QRParser(string digitString)
+        {
+            if (digitString.Length < 52)
+                throw new ArgumentException("Invalid Decimal Digit String");
+            if (digitString.Substring(0, 2) != "90")
+                throw new ArgumentException("Invalid QR Prefix");
+            Version = Byte.Parse(digitString.Substring(2, 2));
+            BinaryPrimitives.WriteUInt16BigEndian(checksum, ushort.Parse(digitString.Substring(4, 5)));
+            byte[] hash = SHA1.HashData(Encoding.ASCII.GetBytes(digitString.Substring(9)));
+            if (!Enumerable.SequenceEqual(hash.Take(2), checksum))
+                throw new InvalidDataException("Invalid Checksum");
+            Keys = (SecurityKey)byte.Parse(digitString.Substring(9, 3));
+            DSK = ToBytes(digitString.Substring(12, 40));
+            if (digitString.Length > 52)
+            {
+                //TODO - Parse TLVs
+            }
+        }
+
+        private static Memory<byte> ToBytes(string byteString)
+        {
+            Memory<byte> ret = new byte[(byteString.Length * 2) / 5];
+            Memory<byte> ptr = ret.Slice(0);
+            for (int i = 0; i < byteString.Length; i+=5)
+            {
+                BinaryPrimitives.WriteUInt16BigEndian(ptr.Slice(0, 2).Span, ushort.Parse(byteString.Substring(i, 5)));
+                ptr = ptr.Slice(2);
+            }
+            return ret;
+        }
+    }
+}
